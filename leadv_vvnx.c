ce2ae1a5cc330e0718c2e03b349460a1d3211e1b
dnagis <vincent.achard@gmail.com>
2017-12-18 08:17:51 +0100

    récupération du device, et quelques pistes en commentaires

--- leadv_vvnx.c
+++ leadv_vvnx.c
@@ -1,9 +1,34 @@
-/** gcc -o leadv leadv_vvnx.c -I/initrd/mnt/dev_save/packages/bluez-5.47 -lbluetooth **/
+/** gcc -o leadv leadv_vvnx.c -I/initrd/mnt/dev_save/packages/bluez-5.47 -lbluetooth 
+ * 
+ * voir lescan en premier. Celui ci c'est pour advertiser pour faire du scan rsp si possible
+ * et passer des paramètres vers l'esp32
+ * 
+ * hci_le_set_advertise_enable() définie dans lib/hci_lib.h -- 
+ * 		Correspond à:
+ * 			Core specs > Core Sys Pkg [BR/EDR Ctrller Vol] Spec Vol. 2 > Part E - HCI func specs > 7-HCI cmds + evts > LE ctrller commands > p 1259 LE Set Advertising Enable Command
+ * 
+ * 
+ * 
+ * **/
 
 #include <stdio.h>
+#include <unistd.h>
+
+#include "lib/bluetooth.h"
+#include "lib/hci.h"
+#include "lib/hci_lib.h"
 
 
 int main()
 {
-fprintf(stderr, "Hello World\n");	
+	int dd;
+	
+	dd = hci_open_dev(0);
+	sleep(5);
+	hci_close_dev(dd);
+	
+	
+	
+	
+	return 0;
 }
