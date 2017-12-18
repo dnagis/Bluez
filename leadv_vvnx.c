dd7f1bd3768c605175a30af5aaba25ec95c85a23
dnagis <vincent.achard@gmail.com>
2017-12-18 10:37:21 +0100

    en cours de set adv param -- le ctrller aime pas, je suppose car les paramètres sont incomplets

--- leadv_vvnx.c
+++ leadv_vvnx.c
@@ -19,7 +19,9 @@
 #include "lib/hci_lib.h"
 
 
-/**hélas la commande n'existe pas dans la librairie bluez, je suis donc obligé de faire ma request à la mano
+/**SET ADVERTTISING DATA
+ * 
+ * la commande n'existe pas dans la librairie bluez, je suis donc obligé de faire ma request à la mano
  * Core Sys Pkg [BR/EDR Ctrller Vol] Spec Vol. 2 > E-HCI func specs > 7-HCI cmds&evts > LE ctrlr cmds > p 1256 LE Set Advertising Data Command
  * 	Pour faire la request cf lib/hci.c (par exemple li.2978 hci_le_set_advertise_enable)
  * 
@@ -51,7 +53,7 @@ int vvnx_hci_le_set_adv_data(int dd)
 		return -1;
 		
 	if (status) {
-	fprintf(stderr, "On a du return parameter va falloir lire status\n");
+	fprintf(stderr, "set_adv_data -> On a du return parameter va falloir lire status\n");
 	return -1;
 	}
 
@@ -59,17 +61,71 @@ int vvnx_hci_le_set_adv_data(int dd)
 	
 }
 
+/**SET ADVERTISING PARAMETERS
+ * 
+ * idem: la commande n'existe pas dans la librairie bluez
+ * ... 7-HCI cmds&evts > LE ctrlr cmds > p 1251 LE Set Advertising Data Command
+ * 	adapté à partir de: lib/hci.c li.2978 hci_le_set_scan_parameters
+ * 
+ ***/
+int vvnx_hci_le_set_adv_parameters(int dd)
+{
+	struct hci_request rq;
+	le_set_advertising_parameters_cp param_cp;
+	uint8_t status;
+	uint8_t bdaddr_type = LE_PUBLIC_ADDRESS;
+	bdaddr_t bdaddr;
+	
+	memset(&param_cp, 0, sizeof(param_cp));
+	param_cp.min_interval = htobs(0x0800);
+	param_cp.max_interval = htobs(0x0800);
+	param_cp.advtype = 0x00;
+	param_cp.own_bdaddr_type = 0x00;
+	param_cp.direct_bdaddr_type = 0x00; //Peer Address Type
+	//direct_bdaddr (bluez) - peer_address (core specs)
+	str2ba("00:00:00:00:00:00", &bdaddr); //adresse "any" ???
+	
+	
+	param_cp.filter = 0x03; //0x03: whitelist only
+	
+	memset(&rq, 0, sizeof(rq));
+	rq.ogf = OGF_LE_CTL;
+	rq.ocf = OCF_LE_SET_ADVERTISING_PARAMETERS;
+	rq.cparam = &param_cp;
+	rq.clen = LE_SET_ADVERTISING_PARAMETERS_CP_SIZE;
+	rq.rparam = &status;
+	rq.rlen = 1;
 
+	if (hci_send_req(dd, &rq, 10000) < 0)
+		return -1;
+
+	if (status) {
+		fprintf(stderr, "set_adv_parameters -> On a du return parameter avec status =  0x%02x\n", status); //Vol.2 Part D Error codes p642
+		return -1;
+	}	
+	
+	return 0;
+}
 
 
 int main()
 {
 	int err, dd;
+	uint8_t bdaddr_type = LE_PUBLIC_ADDRESS;
+	bdaddr_t bdaddr;
 		
 	dd = hci_open_dev(0);
-		
 	
-	/**Ma custom Set Adv Data (because bluez définit les cmds params mais ne fournit pas de fonction)**/
+	/**Whitelist**/
+	str2ba("B8:27:EB:A4:33:13", &bdaddr); //adresse du zero
+	err = hci_le_add_white_list(dd, &bdaddr, bdaddr_type, 1000);
+	fprintf(stderr, "Retour de add_white_list = %i\n", err);
+		
+	/**Ma custom Set Adv Param définie plus haut**/
+	err = vvnx_hci_le_set_adv_parameters(dd);
+	fprintf(stderr, "Retour de set_adv_parameters = %i\n", err);
+	
+	/**Ma custom Set Adv Data définie plus haut**/
 	err = vvnx_hci_le_set_adv_data(dd);
 	fprintf(stderr, "Retour de set_adv_data = %i\n", err);
 	
